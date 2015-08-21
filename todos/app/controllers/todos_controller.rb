513a790b337c4c6c20d96dd17eb07086a349cf47
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-08-21 16:19:32 -0400

    Refactor - create a User object, and use it to check if the user has been signed_in

--- todos/app/controllers/todos_controller.rb
+++ todos/app/controllers/todos_controller.rb
@@ -1,13 +1,13 @@
 class TodosController < ApplicationController
   before_filter :authenticate
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos 
   end
   def new
     @todo = Todo.new
   end
   def create
-    Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
     redirect_to todos_path 
   end
 
