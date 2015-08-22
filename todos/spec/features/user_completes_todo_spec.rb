e9e8318bd9f80df73e8d783f13b161c38c417b7a
Ruben Amortegui <ruben.amortegui@gmail.com>
2015-08-22 13:57:01 -0400

    Add test helper methods

--- todos/spec/features/user_completes_todo_spec.rb
+++ todos/spec/features/user_completes_todo_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 feature "User complete a todo" do
   scenario "successfully" do
     sign_in
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo("Buy milk")
     click_on "Mark completed"
-    expect(page).to have_css ".todos li.completed", text:"Buy milk"
+    expect(page).to display_completed_todo("Buy milk") 
   end 
 end
---
+++ todos/spec/support/features/todo_helper.rb
@@ -0,0 +1,13 @@
+module Features
+  def create_todo(todo_title)
+    click_on "Add a new todo"
+    fill_in "Title", with: todo_title 
+    click_on "Submit"
+  end
+  def display_todo( text )
+    have_css ".todos li", text 
+  end
+  def display_completed_todo( text )
+    have_css ".todos li.completed", text 
+  end
+end
